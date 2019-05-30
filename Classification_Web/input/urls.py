d7c8d8f91b2bdc12413632ec7266361e0b896385
Jimmy Young <yangjufo@gmail.com>
2019-05-30 16:52:34 -0700

    fix a mistakenly changed line

--- Classification_Web/input/urls.py
+++ Classification_Web/input/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index<int:index_pk>', views.index, name='index'),    
-    path('text', views.text, name='text'),
-    path('table', views.table, name='table'),
+    path('text', views.lime, name='text'),
+    path('table', views.lime, name='table'),
     path('ajax/classify<int:classify_pk>', views.classify, name='ajax/classify'),    
 ]
