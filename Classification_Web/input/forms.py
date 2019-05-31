a7581dd3876a7e5f7c4855c4fad80f62f46e117c
Jimmy Young <yangjufo@gmail.com>
2019-05-30 20:40:50 -0700

    remove web dir

--- Classification_Web/input/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class NameForm(forms.Form):
-    input_text = forms.CharField(label='Input text', max_length=300,
-    widget=forms.TextInput(attrs={'style': 'width: 500px; height:30px; word-wrap:break-word; word-break:break-all'}))
