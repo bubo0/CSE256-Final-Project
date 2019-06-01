60e47ae9f9ad31b9c1f4fbe8adb8ecb915422f60
Jimmy Young <yangjufo@gmail.com>
2019-05-31 21:16:52 -0700

    fix bottom of a table

--- Classification_Web/input/views.py
+++ Classification_Web/input/views.py
@@ -59,7 +59,7 @@ def classify_text(searchText, classify_pk):
     exp_clean_save(exp,path,filename)
     return filename
 
-def classify_table(searchText, classify_pk):
+def classify_table(searchText, classify_pk):    
     if classify_pk == 1:
         vect=sen_model.best_estimator_.named_steps.vect
         tfidf=sen_model.best_estimator_.named_steps.tfidf
@@ -81,8 +81,9 @@ def classify_table(searchText, classify_pk):
     features=[vect.get_feature_names()[i] for i in idx]
     explainer=LimeTabularExplainer(train,class_names=clf.classes_,feature_names=features,verbose=False)
     def predict_proba(X):
-        w=clf.coef_[0][idx]
+        w=clf.coef_[0][idx]        
         po=np.dot(X,w)+clf.intercept_[0]
+        print(clf.intercept_[0])
         p=1/(1+np.exp(po))
         ret=zip(p,1-p)
         return np.array(list(ret))
